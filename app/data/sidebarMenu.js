1342a1f179cf682053dc0a0c0a4b2ddcb4174bb8
Deniel Salvacion <denielsalvacion@gmail.com>
2024-07-15 01:02:27 +0800

    Add sidebar menu
    - Add sidebar menu and component

---
+++ app/data/sidebarMenu.js
@@ -0,0 +1,34 @@
+import Home from "../assets/sidebar/home.svg";
+
+const sidebarMenu = [
+  {
+    id: 1,
+    name: "home",
+    icon: Home,
+    route: "/",
+  },
+  // {
+  //     id: 2,
+  //     name: 'dashboard',
+  //     icon: "",
+  //     route: "/dashboard"
+  // },
+  // {
+  //     id: 3,
+  //     name: 'projects',
+  //     icon: "",
+  //     route: "/projects"
+  // },
+  // {
+  //     id: 4,
+  //     name: 'tasks',
+  //     icon: "",
+  //     route: "/tasks"
+  // },
+  // {
+  //     id: 5,
+  //     name: 'home',
+  //     icon: "",
+  //     route: "/"
+  // },
+];
--- app/layouts/Header/Index.jsx
+++ app/layouts/Header/Index.jsx
@@ -1,40 +1,21 @@
 import React from "react";
+import Image from "next/image";
+import Humburger from "../../assets/humburger.svg";
 
-function Header() {
+const Header = ({ onToggleSidebar }) => {
   return (
-    // <div className=" bg-white w-full text-blue-500 border-b border-outline fixed top-0 z-50 ">
-    //   <div className="flex  flex-wrap px-4 py-3 items-center">
-    //     <nav className="flex items-center justify-between w-full mx-auto">
-    //       <div className="flex">
-    //         <div>burger menu</div>
-    //         <div>project name</div>
-    //       </div>
-    //       <div className="flex justify-center items-center">
-    //         <div>Country</div>
-    //         <div>User icon</div>
-    //         <div>Username</div>
-    //       </div>
-    //     </nav>
-    //   </div>
-    // </div>
-    /////
-    // <div className="flex justify-between">
-    //   <div className="flex">
-    //     <div>burger</div>
-    //     <div>Project name</div>
-    //   </div>
-    //   <div className="flex">
-    //     <div>flag</div>
-    //     <div>user</div>
-    //   </div>
-    // </div>
-
     <header className="border-b border-outline fixed top-0 z-50 bg-white w-full">
       <div className="flex flex-wrap px-4 py-3 items-center">
         <nav className="w-full mx-auto flex items-center justify-between">
           <div className="flex justify-center items-center">
-            <div>image</div>
-            <div>Branding</div>
+            <Image
+              src={Humburger}
+              alt="Vercel Logo"
+              width={24}
+              height={24}
+              onClick={onToggleSidebar}
+            />
+            <h1 className="text-xl text-pc-40">Phonebook</h1>
           </div>
           <div className="flex justify-center items-center">
             <div>Country</div>
@@ -44,6 +25,6 @@ function Header() {
       </div>
     </header>
   );
-}
+};
 
 export default Header;
