f01c854badf09b9cbdd8e16fa512308604405d29
Deniel Salvacion <denielsalvacion@gmail.com>
2024-07-31 01:59:54 +0800

    refactor: update year to dynamic year

--- app/layouts/Footer/Index.jsx
+++ app/layouts/Footer/Index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <div className="border-t fixed bottom-0 left-0 w-full p-2 bg-white">
       <span className="text-sm font-medium text-[#43474E">
         {" "}
-        2023 Ingram Micro
+        {year} Placeholder org
       </span>
     </div>
   );
