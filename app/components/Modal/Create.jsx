d39648b7fd59127c63cdc966df749e6a19d69a96
Deniel Salvacion <denielsalvacion@gmail.com>
2024-08-04 03:19:09 +0800

    chore: code cleanup

--- app/components/Modal/Create.jsx
+++ app/components/Modal/Create.jsx
@@ -129,52 +129,6 @@ const CreateModal = ({ isModalOpen, handleModal, closeModal }) => {
             />
           </div>
         </form>
-        {/* <Input
-          placeholder="test title"
-          label="Contact name"
-          name="contact_name"
-        />
-        <div className="flex justify-between mt-5">
-          <div>
-            <Input
-              placeholder="test title"
-              label="Area code"
-              name="area_code"
-              customClassName="w-[150px]"
-            />
-          </div>
-          <div>
-            <Input
-              placeholder="test title"
-              label="Phone number"
-              name="phone_number"
-            />
-          </div>
-        </div>
-
-        <div className="mt-4">
-          <Input placeholder="test title" label="Email" name="email" />
-        </div>
-        <div className="mt-4">
-          <Input placeholder="test title" label="Address" name="address" />
-        </div>
-
-        <div className="flex justify-end mt-7 pb-4">
-          <Button
-            label="Cancel"
-            customClassName="mr-2 w-[80px] h-[40px] text-[#0077D4]"
-            // iconType="leading"
-            // icon={<Image src={AddIcon} alt="Vercel Logo" />}
-            onClick={closeModal}
-          />
-          <Button
-            label="Create contact"
-            style="Primary"
-            customClassName="w-36 h-[40px]"
-            iconType="leading"
-            icon={<Image src={CheckCircle} alt="Vercel Logo" />}
-          />
-        </div> */}
       </Modal>
     </div>
   );
