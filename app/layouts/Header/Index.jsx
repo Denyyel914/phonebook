6997876cf28b7db582f9484de5d6e6cb391c812f
Deniel Salvacion <denielsalvacion@gmail.com>
2024-08-03 22:19:11 +0800

    chore: code cleanup
    - Remove console logs

--- app/layouts/Header/Index.jsx
+++ app/layouts/Header/Index.jsx
@@ -13,16 +13,12 @@ const Header = ({ onToggleSidebar }) => {
 
   const handleModal = () => {
     isSetModalOpen(true);
-    console.log(isModalOpen);
   };
 
   const closeModal = () => {
     isSetModalOpen(false);
   };
 
-  // const handleChange(e) {
-  //   e.
-  // }
   return (
     <header className="border-b border-outline fixed top-0 z-50 bg-white w-full">
       <nav className="w-full mx-auto flex items-center justify-between flex-wrap px-4 py-3">
@@ -49,35 +45,6 @@ const Header = ({ onToggleSidebar }) => {
         </div>
       </nav>
 
-      {/* <Modal
-        isOpen={isModalOpen}
-        onClose={closeModal}
-        title="sample title"
-        titleCustomClass={"text-2xl"}
-      >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum
-          elit et lacinia ornare. Nam ac nulla posuere, cursus risus a,
-        </p>
-
-        <div className="flex justify-end mt-7 pb-4">
-          <Button
-            label="Cancel"
-            customClassName="mr-2 w-[80px] h-[40px] text-[#0077D4]"
-            // iconType="leading"
-            // icon={<Image src={AddIcon} alt="Vercel Logo" />}
-            onClick={closeModal}
-          />
-          <Button
-            label="Create contact"
-            style="Primary"
-            customClassName="w-36 h-[40px]"
-            iconType="leading"
-            icon={<Image src={CheckCircle} alt="Vercel Logo" />}
-            onClick={handleModal}
-          />
-        </div>
-      </Modal> */}
       <CreateModal
         isModalOpen={isModalOpen}
         closeModal={closeModal}
