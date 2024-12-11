49fd7d6ca6bd3f2dbbaef60b4cce341b2ad3c005
Deniel Salvacion <denielsalvacion@gmail.com>
2024-12-11 21:22:15 +0800

    chore: refactor code

--- app/(pages)/auth/page.jsx
+++ app/(pages)/auth/page.jsx
@@ -1,17 +1,22 @@
 "use client";
 import { msalConfig } from "@/app/msal/authConfig.js";
 import { useMsal, useIsAuthenticated } from "@azure/msal-react";
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import { InteractionStatus } from "@azure/msal-browser";
 
 const Logging = () => {
   const { instance, inProgress } = useMsal();
   const isAuthenticated = useIsAuthenticated();
 
-  const initializeInstance = async () => {
-    await instance.initialize();
-    instance.loginRedirect(msalConfig).catch((error) => console.log(error));
-  };
+  // Wrap the function in useCallback to ensure it's memoized and doesn't trigger unnecessary re-renders
+  const initializeInstance = useCallback(async () => {
+    try {
+      await instance.initialize();
+      await instance.loginRedirect(msalConfig);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [instance]);
 
   useEffect(() => {
     if (!isAuthenticated && inProgress === InteractionStatus.None) {
@@ -19,7 +24,7 @@ const Logging = () => {
       localStorage.setItem("isLoggedIn", true);
       initializeInstance();
     }
-  }, [inProgress, initializeInstance, isAuthenticated]); // Include dependencies here
+  }, [isAuthenticated, inProgress, initializeInstance]);
 
   return (
     <>
